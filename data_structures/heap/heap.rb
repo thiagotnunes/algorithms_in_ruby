11e2bc6049621ac6a5230a93fa83aae57e873482
Thiago Nunes <thiagotnunes@gmail.com>
2012-09-03 16:17:35 -0300

    Changing the heaping structure.

--- data_structures/heap/heap.rb
+++ data_structures/heap/heap.rb
@@ -1,18 +1,39 @@
-module Heap
+class Heap
+
+  attr_reader :heap
+
+  def self.max_heap
+    new do |current, parent|
+      current > parent
+    end
+  end
+
+  def insert(element)
+    @heap << element
+    bubble_up(@heap.size - 1)
+    self
+  end
+
+  private
+
+  def initialize(&comparator)
+    @heap = []
+    @comparator = comparator
+  end
+
   def parent_for(current_position)
     base = (current_position - 1)
     base = 0 if base < 0
     base / 2
   end
 
-  def bubble_up(position, array)
+  def bubble_up(position)
     parent = parent_for(position) 
-    if (array[position] <= array[parent])
-      return array
-    end
 
-    swap(array, position, parent)
-    bubble_up(parent, array)
+    if (@comparator.call(@heap[position], @heap[parent]))
+      swap(@heap, position, parent)
+      bubble_up(parent)
+    end
   end
 
   def swap(array, position, parent)
--- data_structures/spec/heap_spec.rb
+++ data_structures/spec/heap_spec.rb
@@ -1,61 +1,21 @@
 require_relative '../heap/heap'
 
-class Dummy
-end
-
 describe "Heap" do
-  before(:each) do
-    @heap = Dummy.new
-    @heap.extend(Heap)
-  end
-
-  context "Retrieving parent" do
-    it "returns 0 for the parent of 0" do
-      @heap.parent_for(0).should == 0
+  context "Max" do
+    before(:each) do
+      @heap = Heap.max_heap
     end
 
-    it "returns 0 for the parent of 1" do
-      @heap.parent_for(1).should == 0
+    context "Inserting" do
+      it "should insert a new element in the first position when heap is empry" do
+        @heap.insert(5)
+        @heap.heap.should == [5]
+      end
+
+      it "should keep the heap structure when inserting new elements" do
+        @heap.insert(5).insert(6).insert(1).insert(7).insert(4)
+        @heap.heap.should == [7,6,1,5,4]
+      end
     end
-
-    it "returns 0 for the parent of 2" do
-      @heap.parent_for(2).should == 0
-    end
-
-    it "returns 1 for the parent of 3" do
-      @heap.parent_for(3).should == 1
-    end
-
-    it "returns 1 for the parent of 4" do
-      @heap.parent_for(4).should == 1
-    end
-
-    it "returns 2 for the parent of 5" do
-      @heap.parent_for(5).should == 2
-    end
-
-    it "returns 2 for the parent of 6" do
-      @heap.parent_for(6).should == 2
-    end
-  end
-
-  context "Bubblying up" do
-    it "bubbles up an element to the beggining when it is the largest" do
-      @heap.bubble_up(3, [4,2,1,7]).should == [7,4,1,2]
-    end
-
-    it "bubbles up an element to the middle when it is larger than all its parents" do
-      @heap.bubble_up(3, [7,2,1,4]).should == [7,4,1,2]
-    end
-
-    it "does nothing when an element is smaller than its immediate parent" do
-      @heap.bubble_up(3, [7,4,1,2]).should  == [7,4,1,2]
-    end
-  end
-
-  context "Max Heapifying" do
-    it "heapifies an ordered array"
-
-    it "heapifies an unordered array"
   end
 end
